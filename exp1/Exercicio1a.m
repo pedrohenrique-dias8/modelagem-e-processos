11c35a689e7b859b423275e94ce6a1565762aeb5
pedrohenrique-dias8 <63249866+pedrohenrique-dias8@users.noreply.github.com>
2023-03-08 22:28:23 -0300

    Add files via upload

--- exp1/Exercicio1a.m
+++ exp1/Exercicio1a.m
@@ -2,14 +2,30 @@
  
  V = (0:0.01:1);
  
- I = zeros(100);
- Ipv = zeros(100);
- Id = zeros(100);
- Ip = zeros(100);
- T = 0;
- for i = 1:1:100
+ I = zeros(1,100);
+ Ipv = zeros(1,100);
+ Id = zeros(1,100);
+ Ip = zeros(1,100);
+ T = 60;
+ 
+ q = 1.602*(10^(-19)); %[C]
+ k = 1.38 *(10^(-23));
+ 
+ Vt = k*T/q;
+ I(1) = 1;
+ 
+ var = 0.5;
+ lambida = 0.6;
+
+ 
+ while var > 0.05
+     i = 2;
      Ipv(i) = (G/Gn)*(Iscn + K1*(T-Tn));
-     Id(i) = Is*(exp((V(i) + Rs*I(i))/Vt*A) - 1);
-     Ip(i) = ((V + Rs*I(i))/Rp);
+     Id(i) = Is*(exp((V(i) + Rs*I(i-1))/Vt*A) - 1);
+     Ip(i) = ((V(i) + Rs*I(i-1))/Rp);
      I(i) = Ipv(i) - Id(i) - Ip(i);
+     
+     I(i) = lambida*I(i) + (1 - lambida)*I(i - 1);
+     var = (I(i) - I(i - 1))*(10^-6);
+     i = i + 1;
  end
