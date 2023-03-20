ab3fd324cbef7e11bde7a2ebb3026393625b2642
pedrohenrique-dias8 <63249866+pedrohenrique-dias8@users.noreply.github.com>
2023-03-20 13:30:49 -0300

    Update Exercicio1b.m

--- exp1/Exercicio1b.m
+++ exp1/Exercicio1b.m
@@ -38,10 +38,25 @@ V = (0:0.01:1);
           Inov = lambida*Inov + (1 - lambida)*Iant;
           var = Inov - Iant;
       end
-      if Inov<0
-         Inov = 0;
-     end
+    
       I200(i) = Inov;
+ end
+ 
+ for i200 = 1:101
+     if I200(i200) < 0
+         break
+     end
+ end
+ 
+ I200f = zeros(1,i200);
+ for i = 1:i200
+     I200f(i) = I200(i);
+ end    
+ 
+  
+ V200 = zeros(1,i200);
+ for i = 2:i200
+     V200(i)=V200(i-1) + 0.01;
  end
 %%
 
@@ -62,12 +77,27 @@ V = (0:0.01:1);
           Inov = lambida*Inov + (1 - lambida)*Iant;
           var = Inov - Iant;
       end
-      if Inov<0
-         Inov = 0;
-     end
+     
       I500(i) = Inov;
  end
 
+ for i500 = 1:101
+     if I500(i500) < 0
+         break
+     end
+ end
+ 
+ I500f = zeros(1,i500);
+ for i = 1:i500
+     I500f(i) = I500(i);
+ end    
+ 
+  
+ V500 = zeros(1,i500);
+ for i = 2:i500
+     V500(i)=V500(i-1) + 0.01;
+ end
+ 
 %%
 G = 1000;
 Vt = k*T/q;
@@ -86,31 +116,54 @@ for i = 1:101
          Inov = lambida*Inov + (1 - lambida)*Iant;
          var = Inov - Iant;
      end
-     if Inov<0
-         Inov = 0;
-     end
+    
      I1000(i) = Inov;
 end 
+
+for i1000 = 1:101
+     if I1000(i1000) < 0
+         break
+     end
+ end
+ 
+ I1000f = zeros(1,i1000);
+ for i = 1:i1000
+     I1000f(i) = I1000(i);
+ end    
+ 
+  
+ V1000 = zeros(1,i1000);
+ for i = 2:i1000
+     V1000(i)=V1000(i-1) + 0.01;
+ end
 %%
 figure;
-plot(V,I200,'b', V, I500, 'g', V, I1000, 'r');
+plot(V200,I200f,'b', V500, I500f, 'g', V1000, I1000f, 'r');
+grid on;
+legend({'200','500','1000'},'Location','northeast');
 xlabel('V');
 ylabel('I');
 
 
-pot200 = zeros(size(V));
-pot500 = zeros(size(V));
-pot1000 = zeros(size(V));
-for i = 1:1:101
-    pot200(i)= V(i)*I200(i);
-    pot500(i)= V(i)*I500(i);
-    pot1000(i)= V(i)*I1000(i);
+pot200 = zeros(size(V200));
+pot500 = zeros(size(V500));
+pot1000 = zeros(size(V1000));
+for i = 1:i200
+    pot200(i)= V200(i)*I200f(i);
+end
+for i = 1:i500
+    pot500(i)= V500(i)*I500f(i);
+end
+for i = 1:i1000
+    pot1000(i)= V1000(i)*I1000f(i);
 end
 
 figure;
-plot(V,pot200,'b', V, pot500, 'g', V, pot1000, 'r');
+plot(V200,pot200,'b', V500, pot500, 'g', V1000, pot1000, 'r');
 xlabel('V');
 ylabel('potencia');
+legend({'200','500','1000'},'Location','northeast');
+grid on;
 
 
 
