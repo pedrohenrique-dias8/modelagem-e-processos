874f77f12b895c14946090ea35494db0db2545d6
pedrohenrique-dias8 <63249866+pedrohenrique-dias8@users.noreply.github.com>
2023-03-20 13:30:09 -0300

    Update Exercicio1a.m

--- exp1/Exercicio1a.m
+++ exp1/Exercicio1a.m
@@ -38,13 +38,31 @@ lambida = 0.35;
           Inov = Ipv - Id - Ip;
           Inov = lambida*Inov + (1 - lambida)*Iant;
           var = Inov - Iant;
-      end
-        if Inov < 0 
-           Inov = 0;
-        end
+      end       
+       
       I0(i) = Inov;
+      
  end
-%%
+ 
+ 
+ for i0 = 1:101
+     if I0(i0) < 0
+         break
+     end
+ end
+ 
+ I0f = zeros(1,i0);
+ for i = 1:i0
+     I0f(i) = I0(i);
+ end    
+ 
+  
+ V0 = zeros(1,i0);
+ for i = 2:i0
+     V0(i)=V0(i-1) + 0.01;
+ end
+ 
+ %%
 
  T = 25+273;
  Vt = k*T/q;
@@ -62,12 +80,26 @@ lambida = 0.35;
           Inov = lambida*Inov + (1 - lambida)*Iant;
           var = Inov - Iant;
       end
-       if Inov < 0 
-           Inov = 0;
-       end
+      
      I25(i) = Inov;
  end
 
+ for i25 = 1:101
+     if I25(i25) < 0
+         break
+     end
+ end
+ 
+I25f = zeros(1,i25);
+ for i = 1:i25
+    I25f(i) = I25(i);
+ end    
+ 
+  
+V25 = zeros(1,i25);
+ for i = 2:i25
+     V25(i)=V25(i-1) + 0.01;
+ end
 %%
 T = 60+273;
 Vt = k*T/q;
@@ -86,29 +118,54 @@ for i = 1:101
          Inov = lambida*Inov + (1 - lambida)*Iant;
          var = Inov - Iant;
      end
-       if Inov < 0 
-           Inov = 0;
-       end
+     
      I60(i) = Inov;
 end 
+
+for i60 = 1:101
+     if I60(i60) < 0
+         break
+     end
+ end
+ 
+ I60f = zeros(1,i60);
+ for i = 1:i60
+     I60f(i) = I60(i);
+ end    
+ 
+  
+ V60 = zeros(1,i60);
+ for i = 2:i60
+     V60(i)=V60(i-1) + 0.01;
+ end
+
+
 %%
 figure;
-plot(V,I60,'b', V, I0, 'g', V, I25, 'r');
+plot(V60,I60f,'b', V0, I0f, 'g', V25, I25f, 'r');
+grid on;
 xlabel('V');
 ylabel('I');
+legend({'60°C', '0°C', '25°C'}, 'Location', 'northeast' )
 
 
-pot0 = zeros(size(V));
-pot25 = zeros(size(V));
-pot60 = zeros(size(V));
-for i = 1:101
-    pot0(i)= V(i)*I0(i);
-    pot25(i)= V(i)*I25(i);
-    pot60(i)= V(i)*I60(i);
+pot0 = zeros(size(V0));
+pot25 = zeros(size(V25));
+pot60 = zeros(size(V60));
+for i = 1:i0
+    pot0(i)= V0(i)*I0f(i);
+end
+for i =1:i25
+    pot25(i)= V25(i)*I25f(i);
+end
+for i =1:i60
+    pot60(i)= V60(i)*I60f(i);
 end
 
 figure;
-plot(V,pot60,'b', V, pot0, 'g', V, pot25, 'r');
+plot(V60,pot60,'b', V0, pot0, 'g', V25, pot25, 'r');
+grid on;
+legend({'60°C', '0°C', '25°C'}, 'Location', 'northeast' )
 xlabel('V');
 ylabel('potencia');
 
