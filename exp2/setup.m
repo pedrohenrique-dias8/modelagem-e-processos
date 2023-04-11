bbbd55e85b69bb2e5906cd35fa71867201ea59d6
pedrohenrique-dias8 <63249866+pedrohenrique-dias8@users.noreply.github.com>
2023-04-11 13:34:22 -0300

    Delete exp2 directory

--- exp2/setup.m
+++
@@ -1,9 +0,0 @@
-%%https://www.scooterzmalaysia.com/products/ninebot-segway-minipro-2-wheel-self-balancing-scooter-hoverboard
-SampleTime = 5; %%[s]
-
-g = 9.8; %%Aceleração da gravidade [m/s^2]
-m = 3; %% massa na ponta do pendulo [Kg]
-M = 10.4; %% massa na base do pendulo [Kg]
-l = 1.2; %% haste do pêndulo [m]
-d = 0.1; %%coeficiente de atrito cinético
-b = 0.1; %%coeficiente de atrito viscoso
